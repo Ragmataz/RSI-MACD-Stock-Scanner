eaec1f260886da5ce8c1dee49211f1be5e4c756a
Ragmataz <deepakupreti100us@gmail.com>
2025-04-18 21:29:43 +0530

    Create telegram_bot.py

---
+++ scanner/telegram_bot.py
@@ -0,0 +1,9 @@
+import os
+import requests
+
+def send_telegram_message(message):
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {"chat_id": chat_id, "text": message, "parse_mode": "HTML"}
+    requests.post(url, data=payload)
