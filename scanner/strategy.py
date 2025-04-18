23df4314b8fa371b4e81d99c02a421b4d7fbecfb
Ragmataz <deepakupreti100us@gmail.com>
2025-04-18 21:55:06 +0530

    Update strategy.py

--- scanner/strategy.py
+++ scanner/strategy.py
@@ -21,38 +21,42 @@ def calculate_rsi_macd(data, fast_length=8, slow_length=16, signal_length=11,
         rs = avg_gain / avg_loss
         data['RSI'] = 100 - (100 / (1 + rs))
         
-        # Determine position based on conditions
-        data['SignalLessMacd'] = data['Signal'] < data['MACD']
-        data['SignalGreaterMacd'] = data['Signal'] > data['MACD']
-        data['RsiOverbought'] = data['RSI'] > overbought
-        data['RsiOversold'] = data['RSI'] < oversold
-        
-        # Create position column
+        # Create position column using exactly the same logic as the TradingView script
         data['Position'] = 0
         
-        # Buy condition: RSI > Overbought AND Signal < MACD
-        buy_condition = (data['RsiOverbought'] & data['SignalLessMacd'])
+        # First row has no previous position to reference
+        if len(data) > 0:
+            data.loc[data.index[0], 'Position'] = 0
+            
+        # Replicate the TradingView logic exactly:
+        # pos = iff(xRSI > Overbought and signal < macd, 1,
+        #        iff(xRSI < Oversold and signal > macd, -1, nz(pos[1], 0)))
+        for i in range(1, len(data)):
+            if data['RSI'].iloc[i] > overbought and data['Signal'].iloc[i] < data['MACD'].iloc[i]:
+                data.loc[data.index[i], 'Position'] = 1
+            elif data['RSI'].iloc[i] < oversold and data['Signal'].iloc[i] > data['MACD'].iloc[i]:
+                data.loc[data.index[i], 'Position'] = -1
+            else:
+                # Keep previous position if conditions aren't met (this is what nz(pos[1], 0) does)
+                data.loc[data.index[i], 'Position'] = data['Position'].iloc[i-1]
         
-        # Sell condition: RSI < Oversold AND Signal > MACD
-        sell_condition = (data['RsiOversold'] & data['SignalGreaterMacd'])
-        
-        # Set positions
-        data.loc[buy_condition, 'Position'] = 1
-        data.loc[sell_condition, 'Position'] = -1
-        
-        # Check for NEW signal in last candle (position changed from previous candle)
+        # Check for a NEW signal in the last candle (position changed from previous)
         signal = None
-        if len(data) >= 2:  # Make sure we have at least 2 candles to compare
+        position_changed = False
+        
+        if len(data) >= 3:  # Need at least 3 candles to detect new signals properly
             current_position = data['Position'].iloc[-1]
             previous_position = data['Position'].iloc[-2]
             
-            # Check if there was a position change (fresh signal)
-            if current_position == 1 and previous_position != 1:
-                signal = 'BUY'
-            elif current_position == -1 and previous_position != -1:
-                signal = 'SELL'
+            # Only report position if it changed in the latest candle
+            if current_position != previous_position:
+                position_changed = True
+                if current_position == 1:
+                    signal = 'BUY'
+                elif current_position == -1:
+                    signal = 'SELL'
         
-        return signal, data
+        return signal, data, position_changed
     except Exception as e:
         print(f"Error in RSI_MACD calculation: {e}")
-        return None, data
+        return None, data, False
